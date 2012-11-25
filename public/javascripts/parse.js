98adf69dd9ffa816c2f0f7b81afcad78d4d85346
root <arerreee@gmail.com>
2012-11-26 01:28:35 +0900

    testtest

---
+++ public/javascripts/parse.js
@@ -0,0 +1,10 @@
+$(function(){
+  $("#submit").click(function(){
+    var message     = $("#message").val();
+    var replacedMsg = message.replace(/\\/g, "/");
+    replacedMsg     = replacedMsg.replace(/\/\//, "");
+    replacedMsg     = "smb:" + replacedMsg;
+    $("#parsed").val(replacedMsg);
+    $("#parsed").select();
+  });
+});
