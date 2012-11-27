2595a480833617f05bbef2e26f53caa4987e3af9
root <arerreee@gmail.com>
2012-11-28 01:24:28 +0900

    converter修正

--- public/javascripts/parse.js
+++ public/javascripts/parse.js
@@ -2,7 +2,7 @@ $(function(){
   $("#submit").click(function(){
     var message     = $("#message").val();
     var replacedMsg = message.replace(/\\/g, "/");
-    replacedMsg     = replacedMsg.replace(/\/\//, "");
+    //replacedMsg     = replacedMsg.replace(/\/\//, "");
     replacedMsg     = "smb:" + replacedMsg;
     $("#parsed").val(replacedMsg);
     $("#parsed").select();
